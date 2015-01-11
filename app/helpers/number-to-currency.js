6f7e4f07dbbde0e2d12e48d077ee5304cc05cc64
Steve Kinney <hello@stevekinney.net>
2015-01-11 14:52:15 -0700

    Allow numeral global

--- app/helpers/number-to-currency.js
+++ app/helpers/number-to-currency.js
@@ -1,7 +1,9 @@
+/* globals numeral */
+
 import Ember from 'ember';
 
 export function numberToCurrency(input) {
   return numeral(input).format('$0.00');
-};
+}
 
 export default Ember.Handlebars.makeBoundHelper(numberToCurrency);
